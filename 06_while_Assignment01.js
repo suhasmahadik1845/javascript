b680ff3e6a4d3f97844c7b531bdab841d0cd217c
suhasmahadik1845 <suhasmahadik1845@gmail.com>
2023-05-28 09:11:38 +0530

    While Loop and Map.
    While Loop and Map.

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,68 @@
+
+console.log(`WAP to print the number from 5 to 15`);
+
+let i=5;
+while (i<=15) {
+    console.log(i);
+    i = i + 1;
+}
+console.log(`___________________________________________________`);
+
+console.log(` `);
+console.log(`WAP to print the number from 50 to 40`);
+let j=50;
+while (j>=40) {
+    console.log(j);
+    j = j - 1;
+}
+console.log(`___________________________________________________`);
+
+console.log(` `);
+console.log(`WAP to first 15 Odd number`);
+let k=0; 
+while (k<=15) {  
+   if (k%2==1) {
+      console.log(k);
+   }
+   k = k + 1; 
+} 
+console.log(`___________________________________________________`);
+
+console.log(` `);
+console.log(`WAP to first 10 Even number`);
+let l=0; 
+while (l<=10) {  
+   if (l%2==0) {
+      console.log(l);
+   }
+   l = l + 2; 
+}
+console.log(`___________________________________________________`);
+
+console.log(` `);
+console.log(`WAP to print table of 5`);
+let m = 5;
+      while(m<=50)
+      {
+         console.log(m);
+         m = m + 5;
+      }
+console.log(`___________________________________________________`);
+
+console.log(` `);
+console.log(`WAP to print table of 10`);
+let n = 10;
+      while(n<=100)
+      {
+         console.log(n);
+         n = n + 10;
+      }
+      console.log(`___________________________________________________`);
+
+      console.log(` `);
+      console.log(`WAP to print table of 10 in reverse order`);
+      let s=100;
+while (s>=10) {
+    console.log(s);
+    s = s - 10;
+}
